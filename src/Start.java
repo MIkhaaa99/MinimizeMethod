90b925dc4ce9d2731c5734b6fbb5d43eaa9fecb9
Mikhail <Mikhail@example.com>
2022-10-04 01:16:52 +0300

    second commit

--- src/Start.java
+++ src/Start.java
@@ -4,22 +4,23 @@ import static java.lang.Math.pow;
 
 public class Start {
 
-    public static double func1(double x) {
+    public static double func(double x) {
         return 0.75 * pow(x, 4) - 2 * pow(x, 3) + 2;
+        //return 2 * pow(x, 2) - 12 * x;
     }
 
     public static int[] svennMethod(double x0, double t) {
         while(true) {
-            if(func1(x0 - t) >= func1(x0) && func1(x0) <= func1(x0 + t)) {
+            if(func(x0 - t) >= func(x0) && func(x0) <= func(x0 + t)) {
                 return new int[] {(int) (x0 - t), (int) (x0 + t)};
             }
-            if(func1(x0 - t) <= func1(x0) && func1(x0) >= func1(x0 + t)) {
+            if(func(x0 - t) <= func(x0) && func(x0) >= func(x0 + t)) {
                 throw new RuntimeException("Функция не является унимодальной!\nЗадайте другую начальную точку x0!");
             }
-            if(func1(x0 - t) >= func1(x0) && func1(x0) >= func1(x0 + t)) {
+            if(func(x0 - t) >= func(x0) && func(x0) >= func(x0 + t)) {
                 x0 = x0 + t;
             }
-            if(func1(x0 - t) <= func1(x0) && func1(x0) <= func1(x0 + t)) {
+            if(func(x0 - t) <= func(x0) && func(x0) <= func(x0 + t)) {
                 x0 = x0 - t;
             }
         }
@@ -32,7 +33,7 @@ public class Start {
         while(i<=N) {
             i++;
             double current = arr[0] + i*((arr[1] - arr[0])/(N+1));
-            if(func1(current) < func1(min)) {
+            if(func(current) < func(min)) {
                 min = current;
             }
         }
@@ -40,24 +41,27 @@ public class Start {
     }
 
     public static double methodTwo(int[] arr) {
-        int x = (arr[0] + arr[1])/2;
-        int L = arr[1] - arr[0];
-        int y = arr[0] + (L/4);
-        int z = arr[1] - (L/4);
-        while(true) {
-            if(func1(y) < func1(x)) {
-                arr[1] = x;
-                x = y;
-
+        double delt = 0.01, eps = 0.01;
+        double a = arr[0];
+        double b = arr[1];
+        while(((b - a) / 2) >= eps) {
+            double x1 = (a + b - delt) / 2;
+            double x2 = (a + b + delt) / 2;
+            if(func(x1) > func(x2)) {
+                a = x1;
+            }
+            if(func(x1) < func(x2)) {
+                b = x2;
             }
         }
+        return (a + b)/2;
     }
 
     public static void main(String[] args) {
         int[] arr = svennMethod(1, 2);
-        System.out.println(Arrays.toString(arr));
-        double result = methodOne(arr);
-        System.out.println(result);
+        System.out.println("Неопределенный интервал: " + Arrays.toString(arr));
+        double result = methodTwo(arr);
+        System.out.println("min = " + result);
         //System.out.println(Arrays.toString(svennMethod(1, 0.2)));
     }
 }
