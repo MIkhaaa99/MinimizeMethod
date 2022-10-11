9e5b87288317fee58dd90a3c40163156a9683fa1
Mikhail <Mikhail>
2022-10-11 22:11:22 +0300

    fourth commit

--- src/Start.java
+++ src/Start.java
@@ -9,9 +9,6 @@ public class Start {
         //return 2 * pow(x, 2) - 12 * x;
     }
 
-    public static double func1(double x) {
-        return 2 * pow(x, 2) - 12 * x;
-    }
     public static int[] svennMethod(double x0, double t) {
         while(true) {
             if(func(x0 - t) >= func(x0) && func(x0) <= func(x0 + t)) {
@@ -61,7 +58,7 @@ public class Start {
         return new double[] {func((a + b)/2), (a + b)/2};
     }
 
-    public static double methodThree(int[] arr) {
+    public static double[] methodThree(int[] arr) {
         double a = arr[0];
         double x = arr[0] + ((3 - Math.sqrt(5))/2) * (arr[1] - arr[0]);
         double y = arr[0] + arr[1] - x;
@@ -80,15 +77,60 @@ public class Start {
             }
 
             if(Math.abs(b - a) < 0.000001) {
-                return (a + b) / 2;
+                return new double[] {func((a + b) / 2), ((a + b) / 2)};
             }
         }
     }
 
+    public static double[] methodFour(int[] arr) {
+        double[] fib = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233};
+        int l = 1;
+        double e = 0.01;
+        int N = 0;
+        while((arr[1] - arr[0]) / l >= fib[N]) {
+            N++;
+        }
+        int k = 0;
+        double a = arr[0];
+        double b = arr[1];
+        double y = a + (fib[N-2]/fib[N]) * (b - a);
+        double z = a + (fib[N-1]/fib[N]) * (b - a);
+
+        while(true) {
+            if(func(y) <= func(z)) {
+                b = z;
+                z = y;
+                y = a + (fib[N - k - 3]/fib[N - k - 1]) * (b - a);
+            }
+            else {
+                a = y;
+                y = z;
+                z = a + (fib[N - k - 2]/fib[N - k - 1]) * (b - a);
+            }
+            if(k != N - 3) {
+                k++;
+            }
+            else {
+                break;
+            }
+        }
+        z = (a + b) / 2;
+        y = z;
+        z = y + e;
+        if(func(y) <= func(z)) {
+            b = z;
+        }
+        else {
+            a = y;
+        }
+        return new double[] {func((a + b) / 2), (a + b) / 2};
+    }
+
     public static void main(String[] args) {
         int[] arr = svennMethod(1, 2);
-        System.out.println("Неопределенный интервал: " + Arrays.toString(arr));git
-        double[] result = methodOne(arr);
+        System.out.println("Неопределенный интервал: " + Arrays.toString(arr));
+        double[] result = methodFour(arr);
+        //double[] result = methodFour(new int[] {0, 10});
         System.out.println("Минимальное значение функции = " + result[0] + " в точке " + result[1]);
         //System.out.println(Arrays.toString(svennMethod(1, 0.2)));
     }
