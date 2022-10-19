c01af754c6b2d0de8291c1b39853f4b80ca5e1e7
Mikhail <Mikhail@example.com>
2022-10-19 21:39:15 +0300

    modify fourth commit

--- src/Start.java
+++ src/Start.java
@@ -1,12 +1,19 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static java.lang.Math.pow;
 
 public class Start {
 
     public static double func(double x) {
-        return 0.75 * pow(x, 4) - 2 * pow(x, 3) + 2;
+        return pow(x, 4) - 2 * x + 4;
+        //return 0.75 * pow(x, 4) - 2 * pow(x, 3) + 2;
         //return 2 * pow(x, 2) - 12 * x;
+    }
+
+    public static double funcDerivative(double x) {
+        return 4 * pow(x, 3) - 2;
     }
 
     public static int[] svennMethod(double x0, double t) {
@@ -83,13 +90,69 @@ public class Start {
     }
 
     public static double[] methodFour(int[] arr) {
-        double[] fib = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233};
-        int l = 1;
+        double[] fib = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765};
+        double l = 0.01;
         double e = 0.01;
         int N = 0;
         while((arr[1] - arr[0]) / l >= fib[N]) {
             N++;
         }
+        int k = 0;
+        List<Double> a = new ArrayList<>();
+        a.add((double) arr[0]);
+        List<Double> b = new ArrayList<>();
+        b.add((double) arr[1]);
+        List<Double> y = new ArrayList<>();
+        double yc = a.get(0) + (fib[N-2]/fib[N]) * (b.get(0) - a.get(0));
+        y.add(yc);
+        List<Double> z = new ArrayList<>();
+        double zc = a.get(0) + (fib[N-1]/fib[N]) * (b.get(0) - a.get(0));
+        z.add(zc);
+        do {
+            if(func(y.get(k)) <= func(z.get(k))) {
+                a.add(a.get(k));
+                b.add(z.get(k));
+                z.add(y.get(k));
+                yc = a.get(k+1) + (fib[N-k-3]/fib[N-k-1]) * (b.get(k+1) - a.get(k+1));
+                y.add(yc);
+            }
+            else {
+                a.add(y.get(k));
+                b.add(b.get(k));
+                y.add(z.get(k));
+                zc = a.get(k+1) + (fib[N-k-2]/fib[N-k-1]) * (b.get(k+1) - a.get(k+1));
+                z.add(zc);
+            }
+            if(k != N-3) {
+                k++;
+                continue;
+            }
+            else {
+                break;
+            }
+        }
+        while(true);
+//        z.add((a.get(N-2)+b.get(N-2)) / 2);
+//        y.add((a.get(N-2)+b.get(N-2)) / 2);
+        y.add((a.get(N-2)+b.get(N-2)) / 2);
+        z.add(y.get(N-1) + e);
+        if(y.get(N-1) <= z.get(N-1)) {
+            a.add(a.get(N-2));
+            b.add(z.get(N-1));
+        }
+        else {
+            a.add(y.get(N-1));
+            b.add(b.get(N-2));
+        }
+        return new double[] {func((a.get(N-1) + b.get(N-1))/2), (a.get(N-1) + b.get(N-1))/2};
+    }
+
+    public static double[] methodFourOld(int[] arr) {
+        double[] fib = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233};
+        int l = 1;
+        double e = 0.01;
+        int N = 0;
+
         int k = 0;
         double a = arr[0];
         double b = arr[1];
